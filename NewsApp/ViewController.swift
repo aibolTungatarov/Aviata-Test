a2982b9ca1e82a1865ed7f6ed8dd23451c6cea93
Aibol Tungatarov <aibolseed@gmail.com>
2020-06-13 23:54:42 +0600

    Initial Commit

---
+++ NewsApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NewsApp
+//
+//  Created by Aibol Tungatarov on 6/13/20.
+//  Copyright © 2020 Aibol Tungatarov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
